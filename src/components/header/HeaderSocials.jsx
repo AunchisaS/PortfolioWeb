4d77136d2cae8641682cf19ff75ecab00c4f3a7e
AunchisaS <creamaunchisa09@gmail.com>
2023-03-27 17:27:13 +0700

    edit linkin link

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className="header__socials">
         <a href="https://github.com/AunchisaS" className='btn-icon' ><img src= {github} alt="" /> </a>
-        <a href="https://www.linkedin.com/in/aunchisa-suwanchatree-359a91264/" className='btn-icon' ><img src= {linkin} alt="" /> </a>
+        <a href="https://www.linkedin.com/in/aunchisa-suwanchatree-4b7b16218/" className='btn-icon' ><img src= {linkin} alt="" /> </a>
         <a href="mailto: aunchisa.suw@gmail.com" className='btn-contact' >GET IN TOUCH</a>
     </div>
   )
